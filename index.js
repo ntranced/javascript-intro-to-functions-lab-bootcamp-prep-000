7f0f03440d1167728ff7cc779d17f731c1a50a2d
Andrew Nguyen <andrew.ntranced@gmail.com>
2018-08-30 21:19:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,14 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
+
+
 function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.") {
     return("I love you, too.")
   }
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
 }

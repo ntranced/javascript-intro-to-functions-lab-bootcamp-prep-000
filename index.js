687c1a4acf74fc8b05ba767aabea8ecf4f0ae0ab
Andrew Nguyen <andrew.ntranced@gmail.com>
2018-08-30 21:25:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.") {
     return("I love you, too.")
   }
-  else if (string === string.toUpperCase()) {
+  else if (string === string.toLowerCase()) {
     return("I can\'t hear you!")
   }
 }

1555f0ef94cf6e0fe24b71847fbc134b90218928
Andrew Nguyen <andrew.ntranced@gmail.com>
2018-08-30 21:16:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function whisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if string === "I love you, Grandma." {
+  if (string === "I love you, Grandma.") {
     return("I love you, too.")
   }
 }
